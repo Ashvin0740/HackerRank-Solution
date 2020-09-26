23b7f8e32eb029c9421aaf74c04d21e32858e352
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-26 11:56:10 +0530

    HelloWorld
    HelloWorls

---
+++ java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, World.");
+         System.out.println("Hello, Java.");
+          
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
