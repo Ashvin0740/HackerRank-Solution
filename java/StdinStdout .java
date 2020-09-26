0e06110a4dd766dfb7a4d9c8523d2d48142125c1
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-26 11:56:57 +0530

    StdinStdout
    StdinStdout

---
+++ java/StdinStdout .java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class StdinStdout {
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      //Complete this line
+      //Complete this line     
+       while(sc.hasNext()){
+        int a=sc.nextInt();          
+        System.out.println(a);
+      }
+      }    
+}
