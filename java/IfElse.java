14eaf9ac63200a441dc96c0e115fd1d3c3c69a45
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-26 11:56:37 +0530

    IfElse
    IfElse

---
+++ java/IfElse.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        String ans;
+        if(n%2==1)ans = "Weird";
+        else
+        {
+            if(n>=2 && n<=5)ans = "Not Weird";
+            else if(n>=5 && n<=20)ans="Weird";
+            else ans = "Not Weird";
+        }
+        System.out.println(ans);
+
+    }
+}
