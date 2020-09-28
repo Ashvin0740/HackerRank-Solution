2b0884d965449fd283ecb8cdf16a0dd296003f63
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-28 10:29:02 +0530

    Day 9
     Recursion 3

---
+++ 30 day of code/Factorial.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Factorial{  
+ public static void main(String args[]){  
+  Scanner sc = new Scanner(System.in);
+  int number = sc.nextInt();
+  int fact=1;  
+  for(int i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println(fact);    
+ }  
+}  
