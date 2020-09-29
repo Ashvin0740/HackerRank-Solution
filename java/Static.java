e7af86393bb6f368308322243a8be87fb182c464
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-09-29 18:52:01 +0530

    Java Static Initializer Block
    Java Static Initializer Block

---
+++ java/Static.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Static{
+
+static boolean flag=true;
+
+static int B,H;
+
+static {
+
+Scanner scan = new Scanner(System.in);
+
+B = scan.nextInt();
+
+H = scan.nextInt();
+
+try{   
+
+if(B<=0 || H<=0){
+
+flag = false;
+
+throw new Exception("Breadth and height must be positive");
+
+} 
+
+}catch(Exception e){
+
+System.out.println(e);
+
+}
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
