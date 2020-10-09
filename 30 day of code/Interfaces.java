3411b98348f07e41ea012b059d4ca459c7c2f138
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-10-09 16:43:47 +0530

    Day 19
     Interfaces

---
+++ 30 day of code/Interfaces.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+    int c = 0;
+    for(int i=1;i<=n; i++){
+        if(n%i == 0)
+        c += i;
+    }
+    return c;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
