1260275d08933a44cf0698fe5ed9eff0f3e71db8
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2020-10-29 16:19:47 +0530

    Java Anagrams
    Java Anagrams

---
+++ java/Java Anagrams.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        char A[] = a.toLowerCase().toCharArray();
+        char B[] = b.toLowerCase().toCharArray();
+
+        if( a.length() != b.length())
+        return false;
+        else
+        {
+            java.util.Arrays.sort(A);
+          java.util.Arrays.sort(B);
+          return java.util.Arrays.equals(A,B);
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
