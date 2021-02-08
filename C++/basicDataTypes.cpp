5fab673aad245e4ab7cb71a3789f31e14ffea461
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-08 18:36:58 +0530

    Basic Data Types
    Basic Data Types

---
+++ C++/basicDataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char ch;
+    float f;
+    double d;
+    
+   cin>>a>>b>>ch>>f>>d;
+   cout << a <<"\n"<< b<<"\n"<<ch<<"\n";
+   printf("%.3f\n", f); 
+    printf("%.9lf", d);
+    return 0;
+}
