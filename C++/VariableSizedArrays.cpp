f93562eb5a85b65d256af87a5006a1306aefb4a1
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-03-06 16:11:24 +0530

    Variable Sized Arrays
    Variable Sized Arrays

---
+++ C++/VariableSizedArrays.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+   int n,q;
+cin >> n>> q;
+int** seq = new int* [n];
+for(int i=0;i<n;i++) {
+    int a;
+    cin>>a;
+    int* b=new int [a];
+    for(int j=0;j<a;j++) {
+        int e;
+        cin>>e;
+        b[j]=e;
+    }
+    *(seq+i)=b;
+}
+for(int i=0;i<q;i++) {
+  int r,s;
+  cin >> r >> s;
+  cout << seq[r][s] << endl;
+}
+return 0;
+
+}
